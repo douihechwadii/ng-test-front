b3ab18fa1795dfa41226a21f606af02a1651a6f9
Wadii Douihech <wadiidouihech@gmail.com>
2025-04-19 23:14:58 +0100

    added a page for home and a logs page

---
+++ src/app/components/log-entry/log-entry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-entry',
+  imports: [],
+  templateUrl: './log-entry.component.html',
+  styleUrl: './log-entry.component.scss'
+})
+export class LogEntryComponent {
+
+}
